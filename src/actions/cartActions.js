4d3aeda8e05fe1ca0c5f843477127f00daa4b71b
Naman <namandalsania12@gmail.com>
2021-08-10 22:37:19 +0530

    create order

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,6 +1,6 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
 
-export const addToCart = (items, product) => (dispatch, getState) => {
+export const addToCart = (product) => (dispatch, getState) => {
     const cartItems = getState().cart.cartItems.slice();
     let alreadyExists = false;
     cartItems.forEach((x) => {
